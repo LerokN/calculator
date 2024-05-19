2dc878b168a242e52e3f3eb7ddd23f578c29bd90
LerokN <167567953+LerokN@users.noreply.github.com>
2024-05-19 14:01:25 +0300

    Загрузила файл

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+int number = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Выберите знак: +, -, *, :, /, %");
+char sign = Convert.ToChar(Console.ReadLine());
+switch (sign)
+{
+    case '+':
+        Console.WriteLine(number + number2);
+        break;
+    case '-':
+        Console.WriteLine(number - number2);
+        break;
+    case '*':
+        Console.WriteLine(number * number2);
+        break;
+    case ':':
+    case '/':
+        Console.WriteLine((double)number / number2);
+        break;
+    case '%':
+        Console.WriteLine(number % number2);
+        break;
+}
